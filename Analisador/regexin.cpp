7ceeb663479fc619cbc4c846e18f582e5eea7027
Dekkard <gustavo.rrff@gmail.com>
2019-05-28 23:42:01 -0300

    Lex não lê mais comentário

--- Analisador/regexin.cpp
+++ Analisador/regexin.cpp
@@ -9,23 +9,47 @@
 using namespace std;
 
 regex regex_number("\\b[0-9]*,?[0-9]*(([Ee][-+])?[0-9]+)\\b");
-//regex regex_keyword("(Program|Var|int|real|Const|Begin|Read|Write|(End.)|End)");
 regex regex_keyword("\\b(if|else|while|return|float|char|void|prnt|int|and|or|not|proc|var|main)\\b");
-regex regex_symbols ("(?![0-9]),(?![0-9])|!=|==|>=|<=|<|>|:=|:|;|=|[(]|[)]|[+]|[-]|<<|>>|\\*\\*|[*]|[/]|[\"]|[{]|[}]");
+regex regex_symbols ("(?![0-9]),(?![0-9])|\\*\\*|<<|>>|!=|==|>=|<=|<|>|:=|:|;|=|[(]|[)]|[+]|[-]|[*]|[/]|[\"]|[{]|[}]");
 regex regex_id("[a-z]([a-zA-z0-9])*");
 regex regex_string("['][a-zA-Z0-9]+[']");
-//regex regex_math_sym("\+|-|\*|\/");
-typedef struct token
-{
-	string value;
-	string type;
+
+typedef struct token{
+	std::string value;
+	std::string type;
 	int len;
+	int pos;
 } token;
-void printMatch_helper(token* lines, string line, regex reg, string type){
+typedef struct posLine{
+	int pos;
+	int line;
+} posLine;
+typedef struct indBlock{
+	posLine start;
+	posLine end;
+	char status;
+} indBlock;
+typedef struct indLine{
+	int pos;
+	char status;
+} indLine;
+
+indLine lineComment;
+indBlock blockComment;
+
+void reset_bc(){
+	blockComment.start.pos = -1;
+	blockComment.start.line = -1;
+	blockComment.end.pos = -1;
+	blockComment.end.line = -1;
+	blockComment.status = 'N';
+	lineComment.pos = -1;
+	lineComment.status = 'N';
+}
+void printMatch_helper(token* lines, string line, regex reg, string type, int li){
     sregex_iterator currentMatch(line.begin(),line.end(),reg);
     sregex_iterator lastMatch;
-    while(currentMatch != lastMatch)
-    {
+    while(currentMatch != lastMatch){
         smatch match = *currentMatch;
 		if(type.compare("id")==0){
 			if(regex_match(match.str(),regex_keyword)){
@@ -33,52 +57,92 @@ void printMatch_helper(token* lines, string line, regex reg, string type){
 				continue;
 			}
 		}
+		if(type.compare("sym")==0){
+			if(match.str().compare(">>")==0){
+				blockComment.start.pos = match.position();
+				blockComment.start.line = li;
+				blockComment.status = 'S';
+				currentMatch++;
+				continue;
+			}
+			if(match.str().compare("<<")==0){
+				blockComment.end.pos = match.position();
+				blockComment.end.line = li;
+				currentMatch++;
+				continue;
+			}
+			if(match.str().compare("**")==0){
+				lineComment.pos = match.position();
+				lineComment.status = 'S';
+				currentMatch++;
+				continue;
+			}
+		}
+		if(lineComment.status == 'S'){
+			if(match.position() > lineComment.pos){
+				currentMatch++;
+				continue;
+			}
+		}
+		if(match.position() > blockComment.start.pos && blockComment.status == 'S'){
+			if(blockComment.end.line < blockComment.start.line){
+				currentMatch++;
+				continue;
+			} else {
+				if(match.position() < blockComment.end.pos){
+					currentMatch++;
+					continue;
+		}}}
 		token aux;
 		aux.value = match.str();
 		aux.type = type;
 		aux.len = match.length();
+		aux.pos = match.position();
 		lines[match.position()] = aux;
         //cout <<"-"<< match.str() <<"-(pos:"<< match.position() << "," << type << ") ";
 		currentMatch++;
     }
 }
-token* printMatch(string line)
-{
+token* printMatch(string line,int li){
 	token *lines = new token[sizeof(token)*line.length()];
-	printMatch_helper(lines,line,regex_keyword,"key");
-	printMatch_helper(lines,line,regex_number,"num");
-	printMatch_helper(lines,line,regex_symbols,"sym");
-	printMatch_helper(lines,line,regex_id,"id");
-	printMatch_helper(lines,line,regex_string,"wrd");
+	printMatch_helper(lines,line,regex_symbols,"sym", li);
+	printMatch_helper(lines,line,regex_keyword,"key", li);
+	printMatch_helper(lines,line,regex_number,"num", li);
+	printMatch_helper(lines,line,regex_id,"id", li);
+	printMatch_helper(lines,line,regex_string,"wrd", li);
+	if(blockComment.status == 'S'){
+		lineComment.pos = -1;
+		lineComment.status = 'N';
+	}
+	if(blockComment.end.pos > blockComment.start.pos && blockComment.end.line >= blockComment.start.line){
+		reset_bc();
+	}
 	return lines;
 }
-int main(int argc, char **argv)
-{
+token** Lex(string filename, int &rows){
 	ifstream file;
-	file.open (argv[1]);
-    if (!file.is_open())
-    {
+	file.open (filename);
+    if (!file.is_open()){
         cout << "Arquivo não encontrado." << endl;
         return 0;
     }
-    string line;
-	int rows = 0;
-	while(getline(file, line))
-    {
+	string line;
+	/*int*/ rows = 0;
+	while(getline(file, line)){
 		rows++;
 	}
 	cout << rows << endl;
 	file.clear();
 	file.seekg(0,ios::beg);
-    cout << "Começo da Analise...\n" << endl;
-	token *Program[rows];
+    cout << "Começo da Analise... ";
+	//token *Program[rows];
+	token **Program = new token*[sizeof(token)*rows];
 	int li = 0;
-    while(getline(file, line))
-    {
+    while(getline(file, line)){
 		token *lines = new token;
 		token *temp, *aux;
 		//cout << "Linha " << li+1 << ": ";
-        temp = printMatch(line);
+        temp = printMatch(line,li+1);
 		//cout << "\n" << endl;
 		for(int i=0,j=0;i<line.length();i++){
 			if(temp[i].value.compare("")!=0){
@@ -100,13 +164,24 @@ int main(int argc, char **argv)
     }
 	cout << "...fim da analise." << endl;
     file.close();
+	return Program;
+}
+void printProg(token** Program, int rows){
 	for(int i=0;i<rows;i++){
 		int j=0;
-		cout << "Linha "<<i+1<<": ";
+		cout << "Linha "<<i+1<<":\t";
 		while(!Program[i][j].value.empty()){
 			cout << Program[i][j++].value << " ";
 		}
 		cout << "" << endl;
 	}
+}
+int main(int argc, char **argv)
+{	
+	reset_bc();
+	string filename = argv[1];
+	int rows;
+	token **Program = Lex(filename,rows);
+    printProg(Program,rows);
 	return 0;
 }
